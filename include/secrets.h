d34a1a11c0ee75ec9251c47605eb323be86772fc
Akoens <43842651+Akoens@users.noreply.github.com>
2021-05-27 12:21:27 +0200

    Delete secrets.h

--- include/secrets.h
+++
@@ -1,5 +0,0 @@
-#define SECRET_SSID "WiFi_opt-out"
-#define SECRET_PASS "Router_Edsersweg_1_123456789"
-#define SECRET_SERVER "192.168.1.179"
-#define SECRET_PORT 443
-#define SECRET_API "/api/send"
